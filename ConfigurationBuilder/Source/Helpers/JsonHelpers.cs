37d49ec88722f14e86c4d342fa0703233d813f16
Jacob Brown <jacob.brown@unity3d.com>
2025-01-12 00:25:03 +0000

    Create JsonHelpers.cs

---
+++ ConfigurationBuilder/Source/Helpers/JsonHelpers.cs
@@ -0,0 +1,15 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace Source.Helpers;
+
+public static class JsonHelpers
+{
+    public static readonly JsonSerializerOptions JsonSerializerOptions = new()
+    {
+        PropertyNameCaseInsensitive = true,
+        AllowTrailingCommas = true,
+        DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull,
+        WriteIndented = true
+    };
+}
