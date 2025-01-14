3a13dd4f6a048efc532a82a30df6d79c649e0c48
Jacob Brown <jacob.brown@unity3d.com>
2025-01-14 16:41:05 +0000

    Use AssemblyService to read embedded resources

---
+++ Common/Services/AssemblyService.cs
@@ -0,0 +1,46 @@
+﻿using System.Reflection;
+using Common.Services.Interfaces;
+
+namespace Common.Services;
+
+public class AssemblyService : IAssemblyService
+{
+    public string? GetEmbeddedResource(string name)
+    {
+        var entryAssembly = GetEntryAssembly();
+        var entryResource = GetResourceFromAssembly(entryAssembly, name);
+        
+        if(entryResource is not null)
+            return entryResource;
+        
+        var commonAssembly = GetCommonAssembly();
+        var commonResource = GetResourceFromAssembly(commonAssembly, name);
+        return commonResource;
+    }
+    
+    public Assembly? GetEntryAssembly() => Assembly.GetEntryAssembly();
+    
+    public Assembly? GetCommonAssembly() => Assembly.GetExecutingAssembly();
+    
+    private static string? GetResourceFromAssembly(Assembly? assembly, string name)
+    {
+        if(assembly is null)
+            return null;
+        
+        var allResources = assembly.GetManifestResourceNames();
+
+        var matchingResources = allResources.Where(x => x.EndsWith(name)).ToList();
+        
+        if(matchingResources.Count == 0)
+            return null;
+        
+        var resource = matchingResources.First();
+            
+        var stream = assembly.GetManifestResourceStream(resource);
+        if (stream is null)
+            return null;
+        
+        using var reader = new StreamReader(stream);
+        return reader.ReadToEnd();
+    }
+}
---
+++ Common/Services/Interfaces/IAssemblyService.cs
@@ -0,0 +1,12 @@
+﻿using System.Reflection;
+
+namespace Common.Services.Interfaces;
+
+public interface IAssemblyService
+{
+    public string? GetEmbeddedResource(string name);
+
+    public Assembly? GetEntryAssembly();
+
+    public Assembly? GetCommonAssembly();
+}
