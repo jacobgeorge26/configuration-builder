de449e4918b3ccfa19def99018a278188c2a4fa5
Jacob Brown <jacob.brown@unity3d.com>
2025-01-14 00:43:28 +0000

    Create SettingsTestHelpers

---
+++ Tests/Extensions/SettingsExtensionsTests.cs
@@ -0,0 +1,189 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using Source.Extensions;
+using Source.Models.CheeseModels;
+using Tests.TestHelpers;
+using Xunit;
+
+namespace Tests.Extensions;
+
+public class SettingsExtensionsTests
+{
+    [Fact]
+    public void AddJsonFile_LoadsValues()
+    {
+        var expected = SettingsTestHelpers.CreateSettings("Cheddar");
+        var (path, fileSystem) = SettingsTestHelpers.SetupJsonSettings(expected);
+        
+        var result = new CheeseSettings().AddJsonFile(path, fileSystem);
+        
+        Assert.Equal(expected.Cheese?.Name, result.Cheese?.Name);
+    }
+    
+    [Fact]
+    public void AddEmbeddedResource_LoadsValues()
+    {
+        var expected = SettingsTestHelpers.CreateSettings("Gouda");
+        var (name, assembly) = SettingsTestHelpers.SetupEmbeddedResourceSettings(expected);
+        
+        var result = new CheeseSettings().AddEmbeddedResource(name, assembly);
+        
+        Assert.Equal(expected.Cheese?.Name, result.Cheese?.Name);
+    }
+    
+    [Fact]
+    public void AddEnvironmentVariables_LoadsValues()
+    {
+        var expected = SettingsTestHelpers.CreateSettings("Brie");
+        var envService = SettingsTestHelpers.SetupEnvVarSettings(expected);
+        
+        var result = new CheeseSettings().AddEnvironmentVariables(envService);
+        
+        Assert.Equal(expected.Cheese?.Name, result.Cheese?.Name);
+    }
+
+    [Theory, MemberData(nameof(OverrideData))]
+    [SuppressMessage("Usage", "xUnit1026:Theory methods should use all of their parameters")]
+    public void AddJsonFile_OverridesCorrectly(CheeseSettings jsonSettings, CheeseSettings resourceSettings, CheeseSettings envVarSettings)
+    {
+        var (path, fileSystem) = SettingsTestHelpers.SetupJsonSettings(jsonSettings);
+        var (name, assembly) = SettingsTestHelpers.SetupEmbeddedResourceSettings(resourceSettings);
+        var envService = SettingsTestHelpers.SetupEnvVarSettings(envVarSettings);
+        
+        var result = new CheeseSettings()
+            .AddEmbeddedResource(name, assembly)
+            .AddEnvironmentVariables(envService)
+            .AddJsonFile(path, fileSystem);
+        
+        Assert.Equal(jsonSettings.Cheese?.Name, result.Cheese?.Name);
+        Assert.Equal(jsonSettings.Cheese?.Price, result.Cheese?.Price);
+        Assert.Equal(jsonSettings.Cheese?.Flavours, result.Cheese?.Flavours);
+        Assert.Equal(jsonSettings.Cheese?.Origin?.Location, result.Cheese?.Origin?.Location);
+    }
+
+    [Theory, MemberData(nameof(OverrideData))]
+    [SuppressMessage("Usage", "xUnit1026:Theory methods should use all of their parameters")]
+    public void AddEmbeddedResource_OverridesCorrectly(CheeseSettings jsonSettings, CheeseSettings resourceSettings, CheeseSettings envVarSettings)
+    {
+        var (path, fileSystem) = SettingsTestHelpers.SetupJsonSettings(jsonSettings);
+        var (name, assembly) = SettingsTestHelpers.SetupEmbeddedResourceSettings(resourceSettings);
+        var envService = SettingsTestHelpers.SetupEnvVarSettings(envVarSettings);
+        
+        var result = new CheeseSettings()
+            .AddEnvironmentVariables(envService)
+            .AddJsonFile(path, fileSystem)
+            .AddEmbeddedResource(name, assembly);
+        
+        Assert.Equal(resourceSettings.Cheese?.Name, result.Cheese?.Name);
+        Assert.Equal(resourceSettings.Cheese?.Price, result.Cheese?.Price);
+        Assert.Equal(resourceSettings.Cheese?.Flavours, result.Cheese?.Flavours);
+        Assert.Equal(resourceSettings.Cheese?.Origin?.Location, result.Cheese?.Origin?.Location);
+    }
+
+    [Theory, MemberData(nameof(OverrideData))]
+    [SuppressMessage("Usage", "xUnit1026:Theory methods should use all of their parameters")]
+    public void AddEnvironmentVariables_OverridesCorrectly(CheeseSettings jsonSettings, CheeseSettings resourceSettings, CheeseSettings envVarSettings)
+    {
+        var (path, fileSystem) = SettingsTestHelpers.SetupJsonSettings(jsonSettings);
+        var (name, assembly) = SettingsTestHelpers.SetupEmbeddedResourceSettings(resourceSettings);
+        var envService = SettingsTestHelpers.SetupEnvVarSettings(envVarSettings);
+        
+        var result = new CheeseSettings()
+            .AddEmbeddedResource(name, assembly)
+            .AddJsonFile(path, fileSystem)
+            .AddEnvironmentVariables(envService);
+        
+        Assert.Equal(envVarSettings.Cheese?.Name, result.Cheese?.Name);
+        Assert.Equal(envVarSettings.Cheese?.Price, result.Cheese?.Price);
+        // Setting empty list is not supported via env var
+        if(envVarSettings.Cheese?.Flavours?.Count > 0)
+            Assert.Equal(envVarSettings.Cheese?.Flavours, result.Cheese?.Flavours);
+        Assert.Equal(envVarSettings.Cheese?.Origin?.Location, result.Cheese?.Origin?.Location);
+    }
+
+    [Fact]
+    public void AddJsonFile_NullValues_DoNotOverride()
+    {
+        var expected = SettingsTestHelpers.CreateSettings("Cheddar", (decimal?)0.99, ["json"], new Farm{ Location = "UK"});
+        var envService = SettingsTestHelpers.SetupEnvVarSettings(expected);
+        
+        var (path, fileSystem) = SettingsTestHelpers.SetupJsonSettings(new CheeseSettings());
+        
+        var result = new CheeseSettings()
+            .AddEnvironmentVariables(envService)
+            .AddJsonFile(path, fileSystem);
+        
+        Assert.Equal(expected.Cheese?.Name, result.Cheese?.Name);
+        Assert.Equal(expected.Cheese?.Price, result.Cheese?.Price);
+        Assert.Equal(expected.Cheese?.Flavours, result.Cheese?.Flavours);
+        Assert.Equal(expected.Cheese?.Origin?.Location, result.Cheese?.Origin?.Location);
+    }
+    
+    [Fact]
+    public void AddEmbeddedResource_NullValues_DoNotOverride()
+    {
+        var expected = SettingsTestHelpers.CreateSettings("Cheddar", (decimal?)0.99, ["json"], new Farm{ Location = "UK"});
+        var (path, fileSystem) = SettingsTestHelpers.SetupJsonSettings(expected);
+        
+        var (name, assembly) = SettingsTestHelpers.SetupEmbeddedResourceSettings(new CheeseSettings());
+        
+        var result = new CheeseSettings()
+            .AddJsonFile(path, fileSystem)
+            .AddEmbeddedResource(name, assembly);
+        
+        Assert.Equal(expected.Cheese?.Name, result.Cheese?.Name);
+        Assert.Equal(expected.Cheese?.Price, result.Cheese?.Price);
+        Assert.Equal(expected.Cheese?.Flavours, result.Cheese?.Flavours);
+        Assert.Equal(expected.Cheese?.Origin?.Location, result.Cheese?.Origin?.Location);
+    }
+    
+    [Fact]
+    public void AddEnvironmentVariables_NullValues_DoNotOverride()
+    {
+        var expected = SettingsTestHelpers.CreateSettings("Cheddar", (decimal?)0.99, ["json"], new Farm{ Location = "UK"});
+        var (name, assembly) = SettingsTestHelpers.SetupEmbeddedResourceSettings(expected);
+        
+        var envService = SettingsTestHelpers.SetupEnvVarSettings(new CheeseSettings());
+
+        var result = new CheeseSettings()
+            .AddEmbeddedResource(name, assembly)
+            .AddEnvironmentVariables(envService);
+        
+        Assert.Equal(expected.Cheese?.Name, result.Cheese?.Name);
+        Assert.Equal(expected.Cheese?.Price, result.Cheese?.Price);
+        Assert.Equal(expected.Cheese?.Flavours, result.Cheese?.Flavours);
+        Assert.Equal(expected.Cheese?.Origin?.Location, result.Cheese?.Origin?.Location);
+    }
+
+    
+    // Test data for overriding values - empty or populated values only
+    // No null values as this is not expected to override
+    public static IEnumerable<object[]> OverrideData =>
+    [
+        // New values
+        [
+            SettingsTestHelpers.CreateSettings("Cheddar", (decimal?)0.99, ["json"], new Farm{ Location = "UK"}), 
+            SettingsTestHelpers.CreateSettings("Gouda", (decimal?)5.23, ["resource"], new Farm{ Location = "Netherlands"}), 
+            SettingsTestHelpers.CreateSettings("Brie", (decimal?)3.48, ["envVar"], new Farm{ Location = "France"})
+        ],
+        // Number
+        [
+            SettingsTestHelpers.CreateSettings("Cheddar", price: (decimal?)0.99), 
+            SettingsTestHelpers.CreateSettings("Gouda", price: -1), 
+            SettingsTestHelpers.CreateSettings("Brie", price: 0)
+        ],
+        // Enumerable
+        [
+            SettingsTestHelpers.CreateSettings("Cheddar", tests: ["json"]), 
+            SettingsTestHelpers.CreateSettings("Gouda", tests: ["resource, resource2"]), 
+            SettingsTestHelpers.CreateSettings("Brie", tests: [])
+        ],
+        // Object
+        [
+            SettingsTestHelpers.CreateSettings("Cheddar", origin: new Farm{ Location = "UK"}), 
+            SettingsTestHelpers.CreateSettings("Gouda", origin: new Farm{ Location = ""}), 
+            SettingsTestHelpers.CreateSettings("Brie", origin: new Farm{ Location = "  "})
+        ]
+    ];
+    
+
+}
---
+++ Tests/TestHelpers/SettingsTestHelpers.cs
@@ -0,0 +1,81 @@
+﻿using System.IO.Abstractions.TestingHelpers;
+using System.Reflection;
+using System.Text;
+using System.Text.Json;
+using Moq;
+using Source.Models.CheeseModels;
+using Source.Services;
+
+namespace Tests.TestHelpers;
+
+internal class SettingsTestHelpers
+{
+    public static CheeseSettings CreateSettings(string name, decimal? price = null, List<string>? tests = null, Farm? origin = null) =>
+    new()
+    {
+        Cheese = new Cheese
+        {
+            Name = name,
+            Price = price,
+            Flavours = tests,
+            Origin = origin
+        }
+    };
+
+    public static (string path, MockFileSystem fileSystem) SetupJsonSettings(CheeseSettings settings)
+    {
+        var json = JsonSerializer.Serialize(settings);
+        return SetupJsonSettings(json);
+    }
+
+    public static (string path, MockFileSystem fileSystem) SetupJsonSettings(string json)
+    {
+        var path = Path.Combine("folder", "settings.json");
+        var fileSystem = new MockFileSystem();
+        fileSystem.AddFile(path, new MockFileData(json));
+        return (path, fileSystem);
+    }
+    
+    public static (string resourceName, Assembly) SetupEmbeddedResourceSettings(CheeseSettings settings)
+    {
+        var json = JsonSerializer.Serialize(settings);
+        return SetupEmbeddedResourceSettings(json);
+    }
+
+    public static (string resourceName, Assembly) SetupEmbeddedResourceSettings(string json)
+    {
+        const string assemblyName = "Flavours", resourceName = "embedded-settings.json";
+
+        var assemblyMock = new Mock<Assembly>();
+        assemblyMock.Setup(x => x.GetName()).Returns(new AssemblyName(assemblyName));
+        
+        var stream = new MemoryStream(Encoding.UTF8.GetBytes(json));
+        assemblyMock.Setup(x => x.GetManifestResourceStream($"{assemblyName}.{resourceName}")).Returns(stream);
+
+        return (resourceName, assemblyMock.Object);
+    }
+    
+    public static IEnvironmentService SetupEnvVarSettings(CheeseSettings settings)
+    {
+        var envVars = new Dictionary<string, string?>
+        {
+            {$"{nameof(CheeseSettings.Cheese)}:{nameof(CheeseSettings.Cheese.Name)}", settings.Cheese?.Name},
+            {$"{nameof(CheeseSettings.Cheese)}:{nameof(CheeseSettings.Cheese.Price)}", settings.Cheese?.Price?.ToString()},
+            {$"{nameof(CheeseSettings.Cheese)}:{nameof(CheeseSettings.Cheese.Origin)}:{nameof(CheeseSettings.Cheese.Origin.Location)}", settings.Cheese?.Origin?.Location}
+        };
+        
+        foreach (var (value, index) in settings.Cheese?.Flavours?.Select((value, i) => ( value, i )) ?? [])
+        {
+            envVars.Add($"{nameof(CheeseSettings.Cheese)}:{nameof(CheeseSettings.Cheese.Flavours)}:{index}", value);
+        }
+
+        return SetupEnvVarSettings(envVars);
+    }
+
+    public static IEnvironmentService SetupEnvVarSettings(Dictionary<string, string?> envVars)
+    {
+        var environmentServiceMock = new Mock<IEnvironmentService>();
+        environmentServiceMock.Setup(x => x.GetEnvironmentVariables()).Returns(envVars);
+        return environmentServiceMock.Object;
+    }
+}
